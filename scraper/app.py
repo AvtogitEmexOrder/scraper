4f1be4522e37dd375e50197ac0cdb0630c057b75
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-10-05 17:54:32 +0800

    rename

--- scraper/app.py
+++ scraper/app.py
@@ -1,14 +1,15 @@
 from scraper.config import setup_config, Config
-from scraper.emex.session import setup_session_emex
+from scraper.emex.session import setup_session_emex, Session
 
 
 class Application():
     def __init__(self) -> None:
         self.config: Config = None
-        self.session = None
+        self.session: Session = None
 
 
 def run_app():
     app = Application()
     setup_config(app, config_path='config.yml')
     setup_session_emex(app)
+    return app
