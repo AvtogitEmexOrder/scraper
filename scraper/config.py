6cb52e44c5e8e0db3b66034ed3e85e993fb5bf88
Vladislav Gorokhov <gvsnbala@gmail.com>
2022-10-13 17:02:43 +0800

    feature: errors

--- scraper/config.py
+++ scraper/config.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 
 import yaml
 
+from scraper.errors import NotFoundFileConfigError, NotFoundKeyConfigError
+
 
 @dataclass
 class Proxy:
@@ -33,24 +35,30 @@ class Config:
 
 
 def setup_config(app, config_path: str):
-    with open(config_path, 'r') as f:
-        raw_config = yaml.safe_load(f)
+    try:
+        with open(config_path, 'r') as f:
+            raw_config = yaml.safe_load(f)
+    except FileNotFoundError:
+        raise NotFoundFileConfigError('NotFound file config')
 
-    app.config = Config(
-        emex=Emex(
-            host=raw_config['emex']['host'],
-            login=raw_config['emex']['login'],
-            password=raw_config['emex']['password'],
-            api_order=raw_config['emex']['api_order'],
-        ),
-        selenium=SettingSelenium(
-            useragent=raw_config['selenium']['useragent'],
-            chromedriver=raw_config['selenium']['chromedriver'],
-        ),
-        proxy=Proxy(
-            login=raw_config['proxy']['login'],
-            password=raw_config['proxy']['password'],
-            host=raw_config['proxy']['host'],
-            port=raw_config['proxy']['port'],
-        ),
-    )
+    try:
+        app.config = Config(
+            emex=Emex(
+                host=raw_config['emex']['host'],
+                login=raw_config['emex']['login'],
+                password=raw_config['emex']['password'],
+                api_order=raw_config['emex']['api_order'],
+            ),
+            selenium=SettingSelenium(
+                useragent=raw_config['selenium']['useragent'],
+                chromedriver=raw_config['selenium']['chromedriver'],
+            ),
+            proxy=Proxy(
+                login=raw_config['proxy']['login'],
+                password=raw_config['proxy']['password'],
+                host=raw_config['proxy']['host'],
+                port=raw_config['proxy']['port'],
+            ),
+        )
+    except KeyError:
+        raise NotFoundKeyConfigError('NotFound Key in file config')
